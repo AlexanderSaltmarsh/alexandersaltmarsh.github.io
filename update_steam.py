8a3bd71875ceb2f05ec580ed04884f91024872e8
AlexanderSaltmarsh <alexander.saltmarsh@gmail.com>
2026-04-03 22:04:34 -0700

    Add Steam activity automation

---
+++ update_steam.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import os
+
+# Get secrets from environment
+API_KEY = os.getenv('STEAM_API_KEY')
+STEAM_ID = os.getenv('STEAM_ID')
+
+# Steam API endpoint for recently played games
+url = f"http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key={API_KEY}&steamid={STEAM_ID}&format=json&count=3"
+
+response = requests.get(url)
+data = response.json()
+
+# Save only what we need to keep it "low-profile"
+games = []
+if "games" in data.get("response", {}):
+    for game in data["response"]["games"]:
+        games.append({
+            "name": game["name"],
+            "image": f"https://cdn.akamai.steamstatic.com/steam/apps/{game['appid']}/header.jpg",
+            "playtime": round(game["playtime_2weeks"] / 60, 1) # hours in last 2 weeks
+        })
+
+with open("recently_played.json", "w") as f:
+    json.dump(games, f)
